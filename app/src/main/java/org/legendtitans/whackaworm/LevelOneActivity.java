24a16949f3c531d10c347f8c613fcc68a71acb7a
mcmahonbr1 <mcmahonbr@s.dcsdk12.org>
2021-02-16 11:18:26 -0700

    Added level one activity

---
+++ app/src/main/java/org/legendtitans/whackaworm/LevelOneActivity.java
@@ -0,0 +1,14 @@
+package org.legendtitans.whackaworm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_one);
+    }
+}
