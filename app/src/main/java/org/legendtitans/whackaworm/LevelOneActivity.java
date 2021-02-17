02e67e9b3911b1dd395103cfb50b70b14576bac9
mcmahonbr1 <mcmahonbr@s.dcsdk12.org>
2021-02-17 11:27:27 -0700

    Added on touch code

--- app/src/main/java/org/legendtitans/whackaworm/LevelOneActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/LevelOneActivity.java
@@ -2,13 +2,30 @@ package org.legendtitans.whackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LevelOneActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level_one);
+        setContentView(R.layout.activity_main);
+
+        ImageView playImageView = (ImageView) findViewById(R.id.playImageView);
+        playImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
 }
