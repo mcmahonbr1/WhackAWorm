7f1096a2841b81510a6992460668057eee1f55f9
mcmahonbr1 <mcmahonbr@s.dcsdk12.org>
2021-02-18 12:16:22 -0700

    Added in play button code

--- app/src/main/java/org/legendtitans/whackaworm/LevelOneActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/LevelOneActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LevelOneActivity extends AppCompatActivity {
 
@@ -17,15 +18,6 @@ public class LevelOneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView playImageView = (ImageView) findViewById(R.id.playImageView);
-        playImageView.setOnTouchListener(new View.OnTouchListener() {
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                Log.i("imageviewandontouchlistener", "playImageView onTouch");
-                return false;
-            }
-        });
     }
 }
--- app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
@@ -2,13 +2,29 @@ package org.legendtitans.whackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView playImageView = (ImageView) findViewById(R.id.playImageView);
+        playImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(),"I was touched", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
